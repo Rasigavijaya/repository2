44053b5a4f48691082e5d05913bf10091fdaa2f7
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-02-08 12:03:22 +0530

    Create set81.c

---
+++ set81.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int a[50],b[50],i,n,j;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d%d",&a[i],&b[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+  c[j]=a[i]=b[i];
+  j++;
+ }
+for(i=0;i<n;i++)
+{
+ printf("%d\n",c[i]);
+}
+}
